e9d304c843977a1ed6ae8f847a7209d5d0c33a2e
Alex Kumundzhiev <alexey.kum@gmail.com>
2018-01-15 03:57:57 +0400

    v.2.1.12

--- client/helpers/facebookAnswerBuilder.js
+++ client/helpers/facebookAnswerBuilder.js
@@ -34,7 +34,6 @@ function standartAnswer (callback) {
                     <title>${site_name}</title>
                     <meta property="og:image" content="${image}"/>
                     <meta property="og:url" content="${siteUrl}"/>
-                    <meta property="og:site_name" content="${site_name}"/>
                     <meta property="og:title" content="${title}"/>         
                     </head>
                     <body>
@@ -88,7 +87,6 @@ exports.facebookAnswer = function (userAgent, url, callback) {
                                 <title>${site_name}</title>
                                 <meta property="og:image" content="${image}"/>
                                 <meta property="og:url" content="${site_url}"/>
-                                <meta property="og:site_name" content="${site_name}"/>
                                 <meta property="og:title" content="${title}"/>
                                 </head>
                                 <body>
